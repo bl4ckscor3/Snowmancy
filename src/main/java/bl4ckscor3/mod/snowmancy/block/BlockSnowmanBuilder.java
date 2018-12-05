5749d1f9a38dadbd4b41e31806bb831f78fe58c1
bl4ckscor3 <donalddani9612@gmx.de>
2018-12-05 21:09:48 +0100

    add block texture and alternatives
    not sure about this

--- src/main/java/bl4ckscor3/mod/snowmancy/block/BlockSnowmanBuilder.java
+++ src/main/java/bl4ckscor3/mod/snowmancy/block/BlockSnowmanBuilder.java
@@ -9,6 +9,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
@@ -26,6 +27,12 @@ public class BlockSnowmanBuilder extends BlockContainer
 		setRegistryName(NAME);
 		setHardness(3.5F);
 		setSoundType(SoundType.STONE);
+	}
+
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state)
+	{
+		return EnumBlockRenderType.MODEL;
 	}
 
 	@Override
