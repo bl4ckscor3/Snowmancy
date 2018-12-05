7d258a86e6ef49708ec322225c1a4c842fdb7f38
bl4ckscor3 <donalddani9612@gmx.de>
2018-12-05 20:01:09 +0100

    add shift+click support

--- src/main/java/bl4ckscor3/mod/snowmancy/container/ContainerSnowmanBuilder.java
+++ src/main/java/bl4ckscor3/mod/snowmancy/container/ContainerSnowmanBuilder.java
@@ -19,6 +19,7 @@ import net.minecraftforge.oredict.OreDictionary;
 public class ContainerSnowmanBuilder extends Container
 {
 	public static final ArrayList<ItemStack> WEAPONS = new ArrayList<>();
+	private TileEntitySnowmanBuilder te;
 
 	/**
 	 * Registers an item to be a wearable weapon for the snowman
@@ -31,6 +32,8 @@ public class ContainerSnowmanBuilder extends Container
 
 	public ContainerSnowmanBuilder(InventoryPlayer playerInv, TileEntitySnowmanBuilder te)
 	{
+		this.te = te;
+
 		//player inventory
 		for(int i = 0; i < 3; i++)
 		{
@@ -53,28 +56,30 @@ public class ContainerSnowmanBuilder extends Container
 			return OreDictionary.itemMatches(new ItemStack(Item.getItemFromBlock(Blocks.SNOW)), stack, false);
 		};
 
+		int slot = 0;
+
 		//hat slot
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 0, 80, 7, 1, (stack) -> {
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 80, 7, 1, (stack) -> {
 			return stack.getItem() instanceof ISnowmanWearable;
 		}));
 		//left eye slot
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 1, 59, 18, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 59, 18, 1, coalValidator));
 		//nose slot
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 2, 80, 28, 1, (stack) -> {
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 80, 28, 1, (stack) -> {
 			return OreDictionary.itemMatches(new ItemStack(Items.CARROT), stack, false);
 		}));
 		//right eye slot
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 3, 101, 18, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 101, 18, 1, coalValidator));
 		//mouth slots (left to right)
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 4, 38, 38, 1, coalValidator));
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 5, 59, 49, 1, coalValidator));
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 6, 80, 49, 1, coalValidator));
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 7, 101, 49, 1, coalValidator));
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 8, 122, 38, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 38, 38, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 59, 49, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 80, 49, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 101, 49, 1, coalValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 122, 38, 1, coalValidator));
 		//top body slots
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 9, 80, 74, 1, snowValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 80, 74, 1, snowValidator));
 		//weapon slot
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 10, 105, 89, 1, (stack) -> {
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 105, 89, 1, (stack) -> {
 			for(ItemStack weapon : WEAPONS)
 			{
 				if(OreDictionary.itemMatches(weapon, stack, false))
@@ -84,8 +89,40 @@ public class ContainerSnowmanBuilder extends Container
 			return false;
 		}));
 		//body slots (middle to bottom)
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 11, 80, 103, 1, snowValidator));
-		addSlotToContainer(new SlotRestricted(te.getInventory(), 12, 80, 132, 1, snowValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 80, 103, 1, snowValidator));
+		addSlotToContainer(new SlotRestricted(te.getInventory(), slot++, 80, 132, 1, snowValidator));
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int index)
+	{
+		ItemStack copy = ItemStack.EMPTY;
+		Slot slot = inventorySlots.get(index);
+
+		if(slot != null && slot.getHasStack())
+		{
+			ItemStack slotStack = slot.getStack();
+
+			copy = slotStack.copy();
+
+			if(index <= 35)
+			{
+				if(!mergeItemStack(slotStack, 36, 36 + te.getInventory().getSizeInventory(), false))
+					return ItemStack.EMPTY;
+			}
+			else if(index >= 36)
+			{
+				if(!mergeItemStack(slotStack, 0, 36, false))
+					return ItemStack.EMPTY;
+			}
+
+			if(slotStack.isEmpty())
+				slot.putStack(ItemStack.EMPTY);
+			else
+				slot.onSlotChanged();
+		}
+
+		return copy;
 	}
 
 	@Override
