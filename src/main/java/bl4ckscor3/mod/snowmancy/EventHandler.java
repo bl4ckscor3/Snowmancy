b97b1aa91699df914a8ee7e72592b6950b23095e
bl4ckscor3 <donalddani9612@gmx.de>
2018-12-30 01:46:37 +0100

    add audio cue when charging up the snowman builder

--- src/main/java/bl4ckscor3/mod/snowmancy/EventHandler.java
+++ src/main/java/bl4ckscor3/mod/snowmancy/EventHandler.java
@@ -5,15 +5,21 @@ import bl4ckscor3.mod.snowmancy.tileentity.TileEntitySnowmanBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 @EventBusSubscriber
 public class EventHandler
 {
+	private static final SoundEvent EGG_SOUND = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.chicken.egg"));
+
 	@SubscribeEvent
 	public static void onProjectileImpactThrowable(ProjectileImpactEvent.Throwable event)
 	{
@@ -22,7 +28,12 @@ public class EventHandler
 			TileEntity te = event.getThrowable().world.getTileEntity(event.getRayTraceResult().getBlockPos());
 
 			if(te instanceof TileEntitySnowmanBuilder)
+			{
+				if(((TileEntitySnowmanBuilder)te).isCraftReady() && ((TileEntitySnowmanBuilder)te).getProgress() < ((TileEntitySnowmanBuilder)te).getMaxProgress())
+					te.getWorld().playSound(null, te.getPos(), EGG_SOUND, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
 				((TileEntitySnowmanBuilder)te).increaseProgress();
+			}
 		}
 	}
 
